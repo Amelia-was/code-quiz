c284d7074fbdd354246f96dd3b508549178fd81e
Deisel <74831846+Amelia-was@users.noreply.github.com>
2020-12-21 23:05:17 -0500

    intitial page setup and styles

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+// Questions
+var question1 = {
+    q: "1. Which is an example of a semantic HTML element?",
+    code: "",
+    a1: "<div>This is my website</div>",
+    a2: "<header>This is my website</header>",
+    a3: "<primary>This is my website</primary>",
+    a4: "<h1>This is my website</h1>",
+    correct: "<header>This is my website</header>"
+}
+
+// Question array
+var questionArray = [question1, question2, question3, question4, question5, question6, question7, question8, question9, question10];
+
+// Loop through questionArray
+// Display question via createElement
+// event listener for click
+// check if selected matches correct
+    // if matches, update score and display correct
+    // if not, display incorrect
+// At end display score
+// allow user to input initials to save high score
+// save score and initials to localStorage
+// display all high scores
+
+// start timer
+// timer is green, turns red close to the end
+// if timer runs out, end quiz
